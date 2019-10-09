75b08881185a62834dd9cec4ac82dfbb91f036ef
Chris West (Faux) <git@goeswhere.com>
2019-10-09 21:58:34 +0100

    capture, using rc/refcell abuse

--- stricter/src/inventory/mod.rs
+++ stricter/src/inventory/mod.rs
@@ -1,3 +1,6 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
 use failure::format_err;
 use failure::Error;
 use failure::ResultExt;
@@ -13,21 +16,22 @@ pub fn load() -> Result<(), Error> {
     run_rust_python()
 }
 
-fn builtin_callback(items: PyIterable, vm: &VirtualMachine) -> PyResult {
-    for item in items.iter(vm)? {
-        let item = item?;
-        for (k, v) in item.downcast::<PyDict>()?.into_iter() {
-            println!("{:?} -> {:?}", k, v);
-        }
-        println!("--");
-    }
-    Ok(vm.get_none())
-}
-
 fn run_rust_python() -> Result<(), Error> {
     let vm = rustpython_vm::VirtualMachine::new(Default::default());
     let scope = vm.new_scope_with_builtins();
-    let callback = vm.ctx.new_rustfunc(builtin_callback);
+    let keys: Rc<RefCell<Vec<String>>> = Default::default();
+    let our_keys = keys.clone();
+    let callback = vm
+        .ctx
+        .new_rustfunc(move |items: PyIterable, vm: &VirtualMachine| -> PyResult {
+            for item in items.iter(vm)? {
+                let item = item?;
+                for (k, _v) in item.downcast::<PyDict>()?.into_iter() {
+                    keys.borrow_mut().push(vm.to_str(&k)?.as_str().to_string());
+                }
+            }
+            Ok(vm.get_none())
+        });
     scope
         .get_locals()
         .set_item("register", callback, &vm)
@@ -44,6 +48,8 @@ fn run_rust_python() -> Result<(), Error> {
     if let Err(err) = vm.run_code_obj(code_obj, scope) {
         print_exception(&vm, &err);
     }
+
+    println!("{:?}", our_keys);
 
     Ok(())
 }

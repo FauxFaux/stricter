674b002af310f55dba116e478130f8a94df889d1
Chris West (Faux) <git@goeswhere.com>
2019-10-10 21:55:09 +0100

    python2json

--- stricter/src/inventory/load_python.rs
+++ stricter/src/inventory/load_python.rs
@@ -8,14 +8,19 @@ use failure::ResultExt;
 use num_traits::cast::ToPrimitive;
 use rustpython_compiler::compile;
 use rustpython_vm::obj::objdict::{PyDict, PyDictRef};
+use rustpython_vm::obj::objfloat::PyFloat;
 use rustpython_vm::obj::objint::PyInt;
+use rustpython_vm::obj::objlist::PyList;
+use rustpython_vm::obj::objlist::PyListRef;
 use rustpython_vm::pyobject::ItemProtocol;
 use rustpython_vm::pyobject::PyIterable;
 use rustpython_vm::pyobject::PyObjectRef;
 use rustpython_vm::pyobject::PyResult;
 use rustpython_vm::pyobject::TryFromObject;
+use rustpython_vm::pyobject::TypeProtocol;
 use rustpython_vm::write_exception;
 use rustpython_vm::VirtualMachine;
+use serde_json::json;
 use serde_json::Value;
 
 pub fn run_rust_python(source: &str) -> Result<Value, Error> {
@@ -36,26 +41,32 @@ pub fn run_rust_python(source: &str) -> Result<Value, Error> {
 
     let code_obj = vm
         .compile(source, compile::Mode::Exec, "<virtual>".to_string())
-        //.map_err(|err| vm.new_syntax_error(&err))?;
+        .map_err(|err| vm.new_syntax_error(&err))
+        .map_err(|err| exception_to_string(err, &vm))
         .with_context(|_| "compiling")?;
 
-    if let Err(err) = vm.run_code_obj(code_obj, scope) {
-        let mut out = Vec::new();
-        write_exception(&mut out, &vm, &err).expect("no io errors on Vec");
-        return Err(err_msg(String::from_utf8_lossy(&out).to_string()));
-    }
+    vm.run_code_obj(code_obj, scope)
+        .map_err(|err| exception_to_string(err, &vm))
+        .with_context(|_| "executing")?;
 
     let docs = our_docs.borrow_mut();
     Ok(Value::Array(docs.to_vec()))
 }
 
+fn exception_to_string(err: PyObjectRef, vm: &VirtualMachine) -> Error {
+    let mut out = Vec::with_capacity(1000);
+    write_exception(&mut out, &vm, &err).expect("no io errors on Vec");
+    err_msg(String::from_utf8_lossy(&out).to_string())
+}
+
 fn to_json(obj: PyObjectRef, vm: &VirtualMachine) -> Result<Value, PyObjectRef> {
     let obj = match obj.downcast::<PyInt>() {
-        Ok(int) => {
-            return Ok(Value::Number(serde_json::Number::from(
-                int.as_bigint().to_i64().expect("TODO: bigint"),
-            )))
-        }
+        Ok(int) => return Ok(json!(int.as_bigint().to_i64().expect("TODO: bigint"))),
+        Err(obj) => obj,
+    };
+
+    let obj = match obj.downcast::<PyFloat>() {
+        Ok(float) => return Ok(json!(float.to_f64())),
         Err(obj) => obj,
     };
 
@@ -64,20 +75,24 @@ fn to_json(obj: PyObjectRef, vm: &VirtualMachine) -> Result<Value, PyObjectRef> 
         Err(obj) => obj,
     };
 
-    let obj = match PyIterable::try_from_object(vm, obj) {
+    let obj = match obj.downcast::<PyList>() {
         Ok(list) => return to_json_array(list, vm),
         Err(obj) => obj,
     };
 
     Err(vm.new_exception(
         vm.ctx.exceptions.type_error.clone(),
-        format!("unexpected type {:?}: {:?}", obj, vm.to_repr(&obj)?),
+        format!(
+            "unexpected type {:?}: {:?}",
+            obj.class().name,
+            &vm.to_repr(&obj)?.as_str()
+        ),
     ))
 }
 
-fn to_json_array(list: PyIterable, vm: &VirtualMachine) -> Result<Value, PyObjectRef> {
+fn to_json_array(list: PyListRef, vm: &VirtualMachine) -> Result<Value, PyObjectRef> {
     let mut ret = Vec::new();
-    for item in list.iter(vm)? {
+    for item in PyIterable::try_from_object(vm, list.into_object())?.iter(vm)? {
         let item = item?;
         ret.push(to_json(item, vm)?);
     }
--- stricter/tests/load_inventory.rs
+++ stricter/tests/load_inventory.rs
@@ -1,10 +1,40 @@
 use failure::Error;
+use serde_json::json;
 
 #[test]
 fn load_first() -> Result<(), Error> {
-    println!(
-        "{:?}",
-        stricter::inventory::load("register({'hello': [5, 5.5]})")?
+    let output = stricter::inventory::load(
+        r"
+for i in range(2):
+    register({f'hello{i}': [i, i + 0.5]})
+",
+    )?;
+
+    assert_eq!(
+        json!([
+            json!({
+                "hello0": [0, 0.5],
+            }),
+            json!({
+                "hello1": [1, 1.5],
+            }),
+        ]),
+        output
     );
+
     Ok(())
+}
+
+#[test]
+fn type_error() {
+    let output = stricter::inventory::load("register({f'hello': [str]})").unwrap_err();
+    assert_eq!(
+        r##"
+Traceback (most recent call last):
+  File "<virtual>", line 1, in <module>
+TypeError: unexpected type "type": "<class \'str\'>"
+"##
+        .trim(),
+        format!("{}", output.find_root_cause()).trim()
+    );
 }

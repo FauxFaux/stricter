c1a07dcd5372c4d769892517e67a97065aedff8a
Chris West (Faux) <git@goeswhere.com>
2019-10-27 16:39:48 +0000

    hurl away python code

--- stricter/src/inventory/mod.rs
+++ stricter/src/inventory/mod.rs
@@ -1,21 +1,23 @@
 use std::collections::HashMap;
 
 use failure::Error;
-use serde_json::Value;
-
-mod load_python;
 
 #[derive(Debug, Clone)]
-struct HostTemplate {
+pub struct HostTemplate {
     connectors: Vec<HashMap<String, String>>,
     tags: HashMap<String, String>,
 }
 
 #[derive(Debug, Clone)]
-struct InventoryTemplate {
+pub struct InventoryTemplate {
     hosts: Vec<HostTemplate>,
 }
 
-pub fn load(input: &str) -> Result<Value, Error> {
-    load_python::run_rust_python(input)
+pub fn load() -> Result<InventoryTemplate, Error> {
+    Ok(InventoryTemplate {
+        hosts: vec![HostTemplate {
+            connectors: vec![],
+            tags: maplit::hashmap! {},
+        }],
+    })
 }
